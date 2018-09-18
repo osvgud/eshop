a40905b93109056827c5e42198db936d35c795b6
osvgud <osvaldas.gudauskas@ktu.edu>
2018-09-18 18:04:27 +0300

    Database and bug fixes

--- config.php
+++ config.php
@@ -1,12 +1,22 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_NAME', 'eshop');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'ec2-54-247-101-191.eu-west-1.compute.amazonaws.com');
+define('DB_NAME', 'd41n7vrltlqqn');
+define('DB_USERNAME', 'wzdevhzxmamrgy');
+define('DB_PASSWORD', 'c6b3637b7d490fdfe3014e687e409f2806cf60268ab8088096c6559ae76a7c2f');
 define('DB_PREFIX', '');
 
 define('NUMBER_OF_ROWS_IN_PAGE', 10);
 
 define('DEFAULT_CONTROLLER', 'index');
+
+//define('DB_SERVER', 'localhost');
+//define('DB_NAME', 'eshop');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', '');
+//define('DB_PREFIX', '');
+//
+//define('NUMBER_OF_ROWS_IN_PAGE', 10);
+//
+//define('DEFAULT_CONTROLLER', 'index');
 
